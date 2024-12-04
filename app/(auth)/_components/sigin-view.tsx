a2066f784a5329aa96fa4fe8bfcc3e4249a4340c
rizwan-m-manzoor <m.rizwan@datics.ai>
2024-12-04 19:33:39 +0500

    updated

--- app/(auth)/_components/sigin-view.tsx
+++ app/(auth)/_components/sigin-view.tsx
@@ -41,9 +41,10 @@ export default function SignInViewPage() {
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
+              {/* &ldquo;This library has saved me countless hours of work and
               helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
+              before.&rdquo; */}
+              Thanks for being awesome.
             </p>
             <footer className="text-sm">Sofia Davis</footer>
           </blockquote>
